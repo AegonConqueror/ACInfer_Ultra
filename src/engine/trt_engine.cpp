9fb407de29d76637a2880ec4bb101df128c885a9
AegonConqueror <jerusalem4755@gmail.com>
2025-06-04 12:27:34 +0800

    根据新版engine接口调整TensorRT推理引擎

--- src/engine/trt_engine.cpp
+++ src/engine/trt_engine.cpp
@@ -85,22 +85,61 @@ public:
     std::shared_ptr<nvinfer1::IRuntime> runtime_ = nullptr;
 };
 
+static ac_tensor_type_e engine_type_convert(TRT::DataType type) {
+    switch (type)
+    {
+    case TRT::DataType::Float:
+        return AC_TENSOR_FLOAT;
+    case TRT::DataType::Float16:
+        return AC_TENSOR_FLOAT16;
+    case TRT::DataType::UInt8:
+        return AC_TENSOR_UINT8;
+    case TRT::DataType::Int32:
+        return AC_TENSOR_INT32;
+    default:
+        LOG_ERROR("unsupported rknn type: %d\n", type);
+        exit(1);
+    }
+}
+
+static ac_engine_attr engine_tensor_attr_encode(
+    const int index,
+    const char *tensor_name,
+    const std::shared_ptr<TRT::Tensor> &trt_tensor) {
+    ac_engine_attr shape;
+
+    shape.index = index;
+    shape.name = tensor_name;
+    shape.n_dims = trt_tensor->ndims();
+    for (int i = 0; i < trt_tensor->ndims(); ++i) {
+        shape.dims[i] = trt_tensor->dims()[i];
+    }
+    shape.n_elems = trt_tensor->numel();
+    shape.size = trt_tensor->numel();
+
+    shape.qnt_zp = 0;
+    shape.qnt_scale = .0f;
+
+    shape.type = engine_type_convert(trt_tensor->type());
+    shape.layout = AC_TENSOR_NCHW;
+
+    return shape;
+}
+
 class TRTEngine : public ACEngine {
 public:
     TRTEngine() {};
 
     ~TRTEngine() override { destory(); };
 
-    error_e init(const std::string &file);
+    error_e create(const std::string &file);
 
     virtual void        Print() override;
-    virtual void        BindingInput(InferenceDataType& inputData) override;
-    virtual void        GetInferOutput(InferenceDataType& outputData, bool sync) override;
+    virtual void        BindingInput(InferenceData& inputData) override;
+    virtual void        GetInferOutput(InferenceData& outputData, bool sync) override;
 
-    virtual std::vector<int>                GetInputShape(int index) override;
-    virtual std::vector<std::vector<int>>   GetOutputShapes() override;
-    virtual std::string                     GetInputType(int index) override;
-    virtual std::vector<std::string>        GetOutputTypes() override;
+    virtual const ac_engine_attrs   GetInputAttrs()     override;
+    virtual const ac_engine_attrs   GetOutputAttrs()    override;
 
 private:
     void destory();
@@ -112,13 +151,17 @@ private:
 
     std::vector<std::shared_ptr<TRT::Tensor>> inputs_;
     std::vector<std::shared_ptr<TRT::Tensor>> outputs_;
-    std::vector<std::string> inputs_name_;
-    std::vector<std::string> outputs_name_;
 
     std::vector<std::shared_ptr<TRT::Tensor>> orderdBlobs_;
 
     std::shared_ptr<EngineContext> context_;
-    std::vector<void* > bindingsPtr_;
+    std::vector<void *> bindingsPtr_;
+
+    uint32_t input_num_;
+    uint32_t output_num_;
+
+    std::vector<ac_engine_attr> input_attrs_;
+    std::vector<ac_engine_attr> output_attrs_;
 };
 
 static TRT::DataType convert_trt_datatype(nvinfer1::DataType dt){
@@ -126,9 +169,31 @@ static TRT::DataType convert_trt_datatype(nvinfer1::DataType dt){
         case nvinfer1::DataType::kFLOAT : return TRT::DataType::Float;
         case nvinfer1::DataType::kHALF  : return TRT::DataType::Float16;
         case nvinfer1::DataType::kINT32 : return TRT::DataType::Int32;
+        case nvinfer1::DataType::kUINT8 : return TRT::DataType::UInt8;
         default:
             LOG_ERROR("Unsupport data type %d", dt);
             return TRT::DataType::Float;
+    }
+}
+
+inline std::string data_type_string(ac_tensor_type_e dt){
+    switch(dt){
+        case AC_TENSOR_FLOAT:   return "Float";
+        case AC_TENSOR_FLOAT16: return "Float16";
+        case AC_TENSOR_UINT8:   return "UInt8";
+        case AC_TENSOR_INT8:    return "Int8";
+        case AC_TENSOR_INT16:   return "Int16";
+        case AC_TENSOR_INT32:   return "Int32";
+        case AC_TENSOR_INT64:   return "Int64";
+        default: return "Unknow";
+    }
+}
+
+inline std::string data_format_string(ac_tensor_fmt_e dt){
+    switch(dt){
+        case AC_TENSOR_NCHW:   return "NCHW";
+        case AC_TENSOR_NHWC:   return "NHWC";
+        default: return "Unknow";
     }
 }
 
@@ -139,8 +204,8 @@ void TRTEngine::destory() {
     this->context_.reset();
     this->inputs_.clear();
     this->outputs_.clear();
-    this->inputs_name_.clear();
-    this->outputs_name_.clear();
+    this->input_attrs_.clear();
+    this->output_attrs_.clear();
     checkCudaRuntime(cudaSetDevice(old_device));
 }
 
@@ -154,18 +219,32 @@ void TRTEngine::Print() {
     LOG_INFO("Infer %p detail", this);
     LOG_INFO("\tBase device: %s", iCUDA::device_description().c_str());
     LOG_INFO("\tMax Batch Size: %d", this->get_max_batch_size());
-    LOG_INFO("\tInputs: %d", inputs_.size());
-    for(int i = 0; i < inputs_.size(); ++i){
-        auto& tensor = inputs_[i];
-        auto& name = inputs_name_[i];
-        LOG_INFO("\t\t%d.%s : shape {%s}, %s", i, name.c_str(), tensor->shape_string(), data_type_string(tensor->type()));
+    LOG_INFO("\tInputs: %d", input_num_);
+    for(int i = 0; i < input_num_; ++i){
+        auto input_attr = input_attrs_[i];
+        std::vector<int64_t> shapes_(input_attr.dims, input_attr.dims + input_attr.n_dims);
+        LOG_INFO(
+            "\t\t%d.%s : shape {%s}, %s, fmt %s, size %d", 
+            i, input_attrs_[i].name.c_str(), 
+            iTools::vector_shape_string(shapes_).c_str(), 
+            data_type_string(input_attr.type).c_str(),
+            data_format_string(input_attr.layout).c_str(),
+            input_attr.size
+        );
     }
 
-    LOG_INFO("\tOutputs: %d", outputs_.size());
-    for(int i = 0; i < outputs_.size(); ++i){
-        auto& tensor = outputs_[i];
-        auto& name = outputs_name_[i];
-        LOG_INFO("\t\t%d.%s : shape {%s}, %s", i, name.c_str(), tensor->shape_string(), data_type_string(tensor->type()));
+    LOG_INFO("\tOutputs: %d", output_num_);
+    for(int i = 0; i < output_num_; ++i){
+        auto output_attr = output_attrs_[i];
+        std::vector<int64_t> shapes_(output_attr.dims, output_attr.dims + output_attr.n_dims);
+        LOG_INFO(
+            "\t\t%d.%s : shape {%s}, %s, fmt %s, size %d", 
+            i, output_attrs_[i].name.c_str(), 
+            iTools::vector_shape_string(shapes_).c_str(), 
+            data_type_string(output_attr.type).c_str(),
+            data_format_string(output_attr.layout).c_str(),
+            output_attr.size
+        );
     }
     LOG_INFO("****************************************************************************");
 }
@@ -174,7 +253,7 @@ void TRTEngine::synchronize() {
     checkCudaRuntime(cudaStreamSynchronize(context_->stream_));
 }
 
-error_e TRTEngine::init(const std::string &file) {
+error_e TRTEngine::create(const std::string &file) {
     auto engine_data = iFile::load_file(file);
     if (engine_data.empty()) {
         return FILE_READ_FAIL;
@@ -191,16 +270,18 @@ error_e TRTEngine::init(const std::string &file) {
     int max_batchsize   = context_->engine_->getMaxBatchSize();
 
     inputs_.clear();
-    inputs_name_.clear();
     outputs_.clear();
-    outputs_name_.clear();
+
+    input_attrs_.clear();
+    output_attrs_.clear();
+
     orderdBlobs_.clear();
     bindingsPtr_.clear();
 
     for (int i = 0; i < nbBindings; ++i) {
         auto dims                   = context_->engine_->getBindingDimensions(i);
         auto type                   = context_->engine_->getBindingDataType(i);
-        const char* bindingName     = context_->engine_->getBindingName(i);
+        const char *bindingName     = context_->engine_->getBindingName(i);
 
         dims.d[0] = max_batchsize;
 
@@ -208,46 +289,29 @@ error_e TRTEngine::init(const std::string &file) {
         newTensor->set_stream(context_->stream_);
         if (context_->engine_->bindingIsInput(i)){
             inputs_.push_back(newTensor);
-            inputs_name_.push_back(bindingName);
+            input_attrs_.push_back(engine_tensor_attr_encode(i, bindingName, newTensor));
         }else{
             outputs_.push_back(newTensor);
-            outputs_name_.push_back(bindingName);
+            output_attrs_.push_back(engine_tensor_attr_encode(i, bindingName, newTensor));
         }
         orderdBlobs_.push_back(newTensor);
     }
     bindingsPtr_.resize(orderdBlobs_.size());
+    input_num_ = input_attrs_.size();
+    output_num_ = output_attrs_.size();
     
     return SUCCESS;
 }
 
-std::vector<int> TRTEngine::GetInputShape(int index) {
-    auto input = inputs_[index];
-    return input->dims();
+const ac_engine_attrs TRTEngine::GetInputAttrs() {
+    return input_attrs_;
 }
 
-std::vector<std::vector<int>> TRTEngine::GetOutputShapes() {
-    std::vector<std::vector<int>> outputshapes;
-    for(auto output : outputs_) {
-        outputshapes.push_back(output->dims());
-    }
-    return outputshapes;
+const ac_engine_attrs TRTEngine::GetOutputAttrs() {
+    return output_attrs_;
 }
 
-std::string TRTEngine::GetInputType(int index){
-    auto input = inputs_[index];
-    return data_type_string(input->type());
-}
-
-std::vector<std::string> TRTEngine::GetOutputTypes(){
-    std::vector<std::string> output_chars;
-    for (auto output : outputs_){
-        output_chars.push_back(data_type_string(output->type()));
-    }
-    return output_chars;
-}
-
-
-void TRTEngine::BindingInput(InferenceDataType& inputData) {
+void TRTEngine::BindingInput(InferenceData& inputData) {
     if (inputs_.size() != inputData.size()) {
         LOG_ERROR("inputs num not match! inputs.size()=%ld, input_num_=%d", inputData.size(), inputs_.size());
         exit(1);
@@ -271,7 +335,7 @@ void TRTEngine::BindingInput(InferenceDataType& inputData) {
     }
 }
 
-void TRTEngine::GetInferOutput(InferenceDataType& outputData, bool sync) {
+void TRTEngine::GetInferOutput(InferenceData& outputData, bool sync) {
     
     int inputBatchSize = inputs_[0]->size(0);
     for(int i = 0; i < context_->engine_->getNbBindings(); ++i){
@@ -291,7 +355,7 @@ void TRTEngine::GetInferOutput(InferenceDataType& outputData, bool sync) {
     for (int i = 0; i < orderdBlobs_.size(); ++i)
         bindingsPtr_[i] = orderdBlobs_[i]->gpu();
 
-    void** bindingsptr = bindingsPtr_.data();
+    void **bindingsptr = bindingsPtr_.data();
     bool execute_result = context_->context_->enqueueV2(bindingsptr, context_->stream_, nullptr);
 
     if(!execute_result){
@@ -329,7 +393,7 @@ std::shared_ptr<ACEngine> create_engine(const std::string &file_path, bool use_p
     }
     
     std::shared_ptr<TRTEngine> Instance(new TRTEngine());
-    if (Instance->init(file_path) != SUCCESS) {
+    if (Instance->create(file_path) != SUCCESS) {
         Instance.reset();
     }
     return Instance;
